5482423f9a2f3a1911b041aec15e5f81d3b7b461
ProCodeGuide <support@procodeguide.com>
2020-07-11 20:32:31 +0530

    First Commit

---
+++ ProCodeGuide.Sample.Blog/Areas/Identity/Data/ApplicationUserClaimsPrincipalFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ProCodeGuide.Sample.Blog.Areas.Identity.Data
+{
+    public class ApplicationUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<SampleAppUser>
+    {
+        public ApplicationUserClaimsPrincipalFactory(
+            UserManager<SampleAppUser> userManager,
+            IOptions<IdentityOptions> options
+            ) : base(userManager, options)
+        {
+
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(SampleAppUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+
+            identity.AddClaim(new Claim("FullName",
+                user.FullName
+                ));
+
+            return identity;
+        }
+    }
+}
---
+++ ProCodeGuide.Sample.Blog/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProCodeGuide.Sample.Blog.Areas.Identity.Data;
+using ProCodeGuide.Sample.Blog.Data;
+
+[assembly: HostingStartup(typeof(ProCodeGuide.Sample.Blog.Areas.Identity.IdentityHostingStartup))]
+namespace ProCodeGuide.Sample.Blog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SampleAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SampleAppContextConnection")));
+
+                services.AddDefaultIdentity<SampleAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SampleAppContext>();
+
+                services.AddScoped<IUserClaimsPrincipalFactory<SampleAppUser>,
+                    ApplicationUserClaimsPrincipalFactory
+                    >();
+            });
+        }
+    }
+}
